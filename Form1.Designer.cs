50541275c9f438d2153907dd3af47e53c227b297
Alexander Gobbato <alexandergobbato@gmail.com>
2021-09-17 19:29:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace CadastroUsuario
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(44, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Alunos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Curso";
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(44, 195);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 4;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(125, 195);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 5;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(206, 195);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(287, 195);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(287, 50);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 8;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(91, 93);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 23);
+            this.txtId.TabIndex = 9;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(91, 122);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(246, 23);
+            this.txtNome.TabIndex = 10;
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(91, 151);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(246, 23);
+            this.txtCurso.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 259);
+            this.Controls.Add(this.txtCurso);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCurso;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroUsuario
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection conexao = new(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\agobb\Downloads\CadastroUsuario\CadastroUsuario\Dados.mdf;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr; // Armazena retornos do SELECT
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+            conexao.Open();
+            MessageBox.Show("Conexão Realizada", "Conexão com Banco");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message.ToString(), "Conexão com Banco");
+            }
+
+        }
+
+        private void Limpar()
+        {
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtCurso.Text = "";
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            /*
+             INSERT INTO ALUNO (NOME, CURSO) VALUES ('ALUNO','CURSO')
+             */
+            int retorno;
+            String cmd_sql;
+            cmd_sql = "INSERT INTO ALUNO (NOME, CURSO) VALUES ('" + txtNome.Text + "','" + txtCurso.Text + "')";
+            cmd = new SqlCommand(cmd_sql, conexao);
+            retorno = cmd.ExecuteNonQuery(); //somente para instruções INSERT/DELETE/UPDATE
+            
+            if(retorno > 0)
+            {
+                MessageBox.Show("Cadastrado com sucesso!", "Cadastro");
+                Limpar();
+            } 
+            else
+            {
+                MessageBox.Show("Cadastro não realizado!", "Cadastro");
+            }
+
+            cmd.Dispose();
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            /*
+             SELECT NOME, CURSO FROM ALUNO WHERE ID = 1
+            */
+                String cmd_sql;
+                cmd_sql = "SELECT NOME, CURSO FROM ALUNO WHERE ID = " + txtId.Text;
+                cmd = new SqlCommand(cmd_sql, conexao);
+                dr = cmd.ExecuteReader();
+                
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    txtNome.Text = dr["NOME"].ToString();
+                    txtCurso.Text = dr["CURSO"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Registro não localizado!", "Consulta");
+                    Limpar();
+                }
+
+            dr.Close();
+            cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Registro não localizado! " + ex.Message.ToString(), "Consulta");
+            }
+            
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                 UPDATE ALUNO SET NOME = 'NOME', CURSO = 'CURSO' WHERE ID = ?
+                 */
+                int retorno;
+                String cmd_sql;
+                cmd_sql = "UPDATE ALUNO SET NOME = '" + txtNome.Text + "', CURSO = '" + txtCurso.Text + "' WHERE ID = " + txtId.Text;
+                cmd = new SqlCommand(cmd_sql, conexao);
+                retorno = cmd.ExecuteNonQuery();
+
+                if (retorno > 0)
+                {
+                    MessageBox.Show("Alteração realizada com sucesso!", "Alterar");
+                }
+                else
+                {
+                    MessageBox.Show("Não foi possível realizar a alteraçãp!", "Alterar");
+                }
+
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Registro não localizado! " + ex.Message.ToString(), "Alterar");
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                 DELETE ALUNO WHERE ID = ?
+                 */
+                int retorno;
+                String cmd_sql;
+                cmd_sql = "DELETE ALUNO WHERE ID = " + txtId.Text;
+                cmd = new SqlCommand(cmd_sql, conexao);
+                retorno = cmd.ExecuteNonQuery();
+
+                if (retorno > 0)
+                {
+                    MessageBox.Show("Excluido com sucesso!", "Excluir");
+                    Limpar();
+                }
+                else
+                {
+                    MessageBox.Show("Registro não localizado!", "Excluir");
+                }
+
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Registro não localizado! " + ex.Message.ToString(), "Excluir");
+            }
+
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
